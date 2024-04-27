cb9e2ca990d510c199cdb11d3b29596cf7ba4439
Johnny Lin <hijohnnylin@gmail.com>
2024-04-27 12:36:47 -0700

    Clean up for pypi

--- neuron-viewer/src/panes/index.js
+++
@@ -1,4 +0,0 @@
-export { default as TopTokens } from "./topTokens"
-export { default as Explanation } from "./explanation"
-export { default as DatasetList } from "./datasetList"
-export { default as SimilarNeurons } from "./similarNeurons"
